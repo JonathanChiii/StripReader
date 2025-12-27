6bef0e501143409d0891255ae49d391a947517b5
JonathanChiii <chi.171@osu.edu>
2025-12-27 18:48:17 -0500

    Changed Camera Sheet to Live View

--- StripReader/Services/StripAnalysis/CameraPreview.swift
+++ StripReader/Services/StripAnalysis/CameraPreview.swift
@@ -1,26 +1,36 @@
-////
-////  CameraPreview.swift
-////  StripReader
-////
-////  Created by jOnAtHaN Chi on 11/23/25.
-////
 //
-//import Foundation
-//import SwiftUI
-//import AVFoundation
+//  CameraPreview.swift
+//  StripReader
 //
-//struct CameraPreview: UIViewRepresentable {
-//    let cameraService: CameraService
+//  Created by jOnAtHaN Chi on 12/22/25.
 //
-//    func makeUIView(context: Context) -> UIView {
-//        let view = UIView()
-//
-//        let layer = cameraService.makePreviewLayer()
-//        layer.frame = UIScreen.main.bounds
-//        view.layer.addSublayer(layer)
-//
-//        return view
-//    }
-//
-//    func updateUIView(_ uiView: UIView, context: Context) {}
-//}
+
+import SwiftUI
+import AVFoundation
+
+struct CameraPreview: UIViewRepresentable {
+
+    let session: AVCaptureSession
+
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+
+        let layer = AVCaptureVideoPreviewLayer(session: session)
+        layer.videoGravity = .resizeAspectFill
+        layer.connection?.videoOrientation = .portrait
+
+        view.layer.addSublayer(layer)
+
+        DispatchQueue.main.async {
+            layer.frame = view.bounds
+        }
+
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {
+        guard let layer = uiView.layer.sublayers?.first as? AVCaptureVideoPreviewLayer else { return }
+        layer.frame = uiView.bounds
+    }
+}
+
